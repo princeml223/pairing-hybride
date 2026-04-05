53a391fd5fcd8f3e502de6fe8b95e208cdde82bb
princeml223 <ahmadoumahamane5@gmail.com>
2026-04-05 12:43:18 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "hybride-core-session",
+  "version": "1.0.0",
+  "main": "api/generate.js",
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.6.0",
+    "pino": "^8.19.0",
+    "pastebin-api": "^5.1.0",
+    "axios": "^1.6.7"
+  }
+}
